e113e5b18dba81eff619813c66b856601dcb5914
biunovich <biunovich@gmail.com>
2016-07-20 10:30:45 +0600

    Bug fixed and added Makefile

--- chdir.c
+++ chdir.c
@@ -1,8 +1,9 @@
 #include "mylib.h"
-void chan_dir(WINDOW *win,char **dir_o,char* path,int * sizedir,int *ptr,int *shift)
+char ** chan_dir(WINDOW *win,char ** dir_t,char* path,int * sizedir,int *ptr,int *shift)
 {
+	char ** dir_o;
 	chdir(path);
-	chdir(dir_o[*ptr+*shift-1]);
+	chdir(dir_t[*ptr+*shift-1]);
 	getcwd(path,100);
 	wclear(win);
 	box(win,0,0);
@@ -14,6 +15,9 @@ void chan_dir(WINDOW *win,char **dir_o,char* path,int * sizedir,int *ptr,int *sh
 		i++;
 	}
 	*sizedir = i;
+	dir_o = calloc(sizeof(char*),i);
+	for (j=0;j<*sizedir;j++)
+		dir_o[j] = calloc(sizeof(char),100);
 	dir = opendir(path);
 	i=0;
 	while ((entry = readdir(dir)) != NULL)
@@ -29,11 +33,12 @@ void chan_dir(WINDOW *win,char **dir_o,char* path,int * sizedir,int *ptr,int *sh
 	*shift = 0;
 	wmove(win,1,1);
 	wrefresh(win);
+	return dir_o;
 }
 void change_dir(WINDOW *win1,WINDOW * win2)
 {
 	if (winlock == TRUE)
-		chan_dir(win1,dir1,path1,&sizedir1,&ptr1,&shift1);
+		dir1 = chan_dir(win1,dir1,path1,&sizedir1,&ptr1,&shift1);
 	else if (winlock == FALSE)
-		chan_dir(win2,dir2,path2,&sizedir2,&ptr2,&shift2);
+		dir2 = chan_dir(win2,dir2,path2,&sizedir2,&ptr2,&shift2);
 }
